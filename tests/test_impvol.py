8868acdd09c520372ff168c085720ad70044ce0f
Stanislav Khrapov <khrapovs@gmail.com>
2015-02-20 14:03:20 -0500

    Relative import in testing

--- tests/test_impvol.py
+++ tests/test_impvol.py
@@ -8,7 +8,7 @@ from __future__ import print_function, division
 import unittest as ut
 import numpy as np
 
-from impvol import imp_vol, find_largest_shape, lfmoneyness
+from ..impvol import imp_vol, find_largest_shape, lfmoneyness
 
 
 class ImpVolTestCase(ut.TestCase):
